0d4c07b671ea05910a6e2d2652d5a68cdda4ad0a
michaelchum <michaelhochum@gmail.com>
2013-11-14 13:03:40 -0500

    Added 5 most viewed categories to index

--- rango/views.py
+++ rango/views.py
@@ -36,10 +36,12 @@ def index(request):
 	# Order the categories by no. likes in descending order
 	# Retrieve the top 5 only - or all if less than 5.
 	# Place the list in our context_dict dictionary which will be passed to the template engine
-	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
+	category_most_likes = Category.objects.order_by('-likes')[:5]
+	category_most_views = Category.objects.order_by('-views')[:5] 
+	context_dict = {'categories_likes': category_most_likes, 'categories_views': category_most_views}
 
-	encodeURL(category_list)
+	encodeURL(category_most_likes)
+	encodeURL(category_most_views)
 
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier
