0c94c4f974ff22bef142392d089e940d50e00ff3
michaelchum <michaelhochum@gmail.com>
2013-11-19 15:16:44 -0500

    Fixed add_category view, added add_category template and added add_page view

--- rango/views.py
+++ rango/views.py
@@ -108,12 +108,46 @@ def add_category(request):
 			# The supplied form contained errors - just print them to the terminal.
 			print form.errors
 	else:
-	# If the request was not a POST, display the form to enter details.
-	form = CategoryForm()
+		# If the request was not a POST, display the form to enter details.
+		form = CategoryForm()
 
 	# Bad form (or form details), no form supplied...
 	# Render the form with error messages (if any).
 	return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+	context = RequestContext(request)
+
+	category_name = decode_url(category_name_url)
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+	if form.is_valid():
+		# This time we cannot commit straight away.
+		# Not all fields are automatically populated!
+		page = form.save(commit=False)
+
+		# Retrieve the associated Category object so we can add it.
+		cat = Category.objects.get(name=category_name)
+		page.category = cat
+
+		# Also, create a default value for the number of views.
+		page.views = 0
+
+		# With this, we can then save our new model instance.
+		page.save()
+
+		# Now that the page is saved, display the category instead.
+		return category(request, category_name_url)
+	else:
+		print form.errors
+	else:
+		form = PageForm()
+
+	return render_to_response( 'rango/add_page.html',
+		{'category_name_url': category_name_url,
+		'category_name': category_name, 'form': form},
+		context)
 
 
 
