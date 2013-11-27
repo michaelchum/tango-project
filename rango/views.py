4298933cc1865d92243a11176fd835c0a6442f8d
michaelchum <michaelhochum@gmail.com>
2013-11-26 19:35:11 -0500

    Adde base template and extended category.html from base.html

--- rango/views.py
+++ rango/views.py
@@ -257,11 +257,11 @@ def restricted(request):
 
 @login_required
 def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
-    logout(request)
+	# Since we know the user is logged in, we can now just log them out.
+	logout(request)
 
-    # Take the user back to the homepage.
-    return HttpResponseRedirect('/rango/')
+	# Take the user back to the homepage.
+	return HttpResponseRedirect('/rango/')
 
 
 
