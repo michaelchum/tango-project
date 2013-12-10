62ba3d3b346a9ffdb071fd92c84f0c90f096f120
michaelchum <michaelhochum@gmail.com>
2013-12-09 22:50:06 -0500

    Added search engine

--- tango_project/settings.py
+++ tango_project/settings.py
@@ -57,7 +57,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+MEDIA_ROOT = MEDIA_PATH
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
